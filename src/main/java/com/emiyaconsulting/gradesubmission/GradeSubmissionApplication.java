ed1eb2ae10e351bcba3dd4396c1a70f496150293
Rob Ranf <rob@emiyaconsulting.com>
2024-02-17 17:56:34 -0800

    init

---
+++ src/main/java/com/emiyaconsulting/gradesubmission/GradeSubmissionApplication.java
@@ -0,0 +1,12 @@
+package com.emiyaconsulting.gradesubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeSubmissionApplication.class, args);
+    }
+}
