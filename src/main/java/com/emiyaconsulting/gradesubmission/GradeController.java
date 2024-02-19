6a0006838e66421fc976d62281b4c0674adf228a
Rob Ranf <rob@emiyaconsulting.com>
2024-02-18 18:46:40 -0800

    adding grade model

--- src/main/java/com/emiyaconsulting/gradesubmission/GradeController.java
+++ src/main/java/com/emiyaconsulting/gradesubmission/GradeController.java
@@ -1,12 +1,18 @@
 package com.emiyaconsulting.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
     @GetMapping("/grades")
-    public String sayHell() {
+    public String getGrades(Model model) {
+        Grade madisonEnglish = new Grade("Madison", "English", "A-");
+        model.addAttribute("madisonEnglish", madisonEnglish);
+
+        Grade oliviaMath = new Grade("Olivia", "Math", "C");
+        model.addAttribute("olivaMath", oliviaMath);
         return "grades";
     }
 }
